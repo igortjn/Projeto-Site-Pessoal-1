3a32578400d10acdb17b3d3097addd62df73ae17
Igor Trajano <igortjn@gmail.com>
2019-12-10 17:56:36 -0300

    Arquivos HTML, CSS e Imagens

---
+++ js/style.js
@@ -0,0 +1,10 @@
+function maiuscula () {
+    var letra = document.getElementById("nome").value;
+    letra = letra.toUpperCase ();
+    document.getElementById("nome").value = letra;
+}
+function maiuscula2 () {
+    var letra = document.getElementById("area").value;
+    letra = letra.toUpperCase ();
+    document.getElementById("area").value = letra;
+}
